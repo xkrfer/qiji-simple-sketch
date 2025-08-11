8c97f246d2b5c9be7e7d33e4a3cbac0369a07805
xkrfer <xkrfer@gmail.com>
2025-08-11 18:16:45 +0800

    feat: 添加组件配置文件，更新依赖项，优化样式和工具函数

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,21 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import copy from 'copy-to-clipboard';
-import { toast } from 'sonner';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function getBaseUrl() {
-  return '';
-}
-
-export function copyText(text: string) {
-  copy(text);
-  toast.success('复制成功');
-}
-
-export function encodeBase64(text: string) {
-  return btoa(text);
+  return twMerge(clsx(inputs))
 }
